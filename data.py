23fab3da46850ae0cb812c4acf4cfba0add759e9
mahbubcubd <mahbubcu14@gmail.com>
2017-06-01 15:26:36 +0600

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,17 @@
+# Parsing json from directory 
+
 
 import json
 m='file directory like D:\json\js.json'
 n=json.load(m)
-n
+print n;
+
+
+#parsing json data from url for python 2.7
+import urllib, json
+url = "http://maps.googleapis.com/maps/api/geocode/json?address=google"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data
+
+#parsing json data from url for python 3
